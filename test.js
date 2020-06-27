9c75007b0b0af13ced96b1ddb911e84004983591
midgar27 <rgodwin81@gmail.com>
2020-06-27 21:33:04 +0100

    End of Section 110

--- test.js
+++ test.js
@@ -1,8 +1,28 @@
-// interpreter vs compiler
-function someCalculation(x, y) {
-  return x + y;
+// // inline caching
+// function findUser(user) {
+//   return `found ${user.firstName} ${user.lastName}`
+// }
+
+// const userData = {
+//   firstName: 'Johnson',
+//   lastName: 'Junior'
+// }
+
+// console.log(findUser(userData))
+
+// hidden classes
+
+function Animal(x, y) {
+  this.x = x;
+  this.y = y;
 }
 
-for (let i = 0; i < 1000; i++) {
-  someCalculation(5, 4);
-}
+const obj1 = new Animal(1, 2);
+const obj2 = new Animal(3, 4);
+
+// obj1.a = 30;
+// obj1.b = 100;
+// obj2.b = 30;
+// obj2.a = 100;
+
+delete obj1.x;
